f02504460ceecc40af9fb7e3bfeb78693951f44e
michal.tymula <michal.tymula@gmail.com>
2017-12-19 16:52:18 +0100

    added images catalog, and some initial components

--- js/app.jsx
+++ js/app.jsx
@@ -1,1 +1,58 @@
-console.log("SIEMA!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router,
+  Route,
+  Link,
+  IndexLink,
+  IndexRoute,
+  hashHistory
+} from 'react-router';
+
+
+class Main extends React.Component{
+  render(){
+    return <div id="main">
+            <header>
+              <img src="../images/logo-zajebiste.png" alt="to jest logo"/>
+              <nav>
+              <ul>
+                <li>
+                  <IndexLink to="/" >Home</IndexLink>
+                </li>
+                <li>
+                  <IndexLink to="/blog" >Blog</IndexLink>
+                </li>
+                <li>
+                  <IndexLink to="/about" >About</IndexLink>
+                </li>
+              </ul>
+              </nav>
+            </header>
+            
+
+    </div>
+  }
+}
+
+class Homepage extends React.Component {
+  render(){
+    return <div>
+              {this.props.children}
+           </div>
+  }
+}
+class App extends React.Component{
+  render(){
+    return <Router history={hashHistory}>
+      <Route path="/" component={Homepage}>
+        <IndexRoute component={Main}/>
+      </Route>
+    </Router>
+  }
+}
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+});
