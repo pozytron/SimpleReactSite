1b6c46a38bd616139a8cc4b61e714f7defaa7787
michal.tymula <michal.tymula@gmail.com>
2017-12-19 16:25:01 +0100

    initial commit ;)

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+   entry: ["whatwg-fetch","./js/zadanie00.jsx"],
+   output: {
+       filename: "./js/out.js"
+   },
+	devServer: {
+		inline: true,
+		contentBase: "./",
+		port: 3001
+	},
+   watch: true,
+   devtool: 'source-map',
+
+   module: {
+       loaders: [{
+           test: /\.jsx$/,
+           exclude: /node_modules/,
+           loader: 'babel-loader',
+           query: {
+               presets: ['es2015', 'stage-2', 'react']
+           }
+       }]
+   },
+
+   // plugins: [
+   //     new UglifyJsPlugin({
+   //         sourceMap: true,
+   //         exclude: /node_modules/
+   //     })
+   // ]
+};
