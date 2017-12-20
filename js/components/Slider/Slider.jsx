831d7b7f6c2f795497617ce464277ed2595d0d97
michal.tymula <michal.tymula@gmail.com>
2017-12-20 15:28:14 +0100

    created slider based on bootstrap documentation

--- js/components/Slider/Slider.jsx
+++ js/components/Slider/Slider.jsx
@@ -2,14 +2,53 @@ import React from 'react';
 
 class Slider extends React.Component {
   render(){
-    return <section className="row">
-      <ul>
-        <li><h2>Pierwszy slajd</h2><button> Kliknij se!</button></li>
-        <li><h2>Drugi slajd</h2><button> Kliknij se!</button></li>
-        <li><h2>Trzeci slajd</h2><button> Kliknij se!</button></li>
-        <li><h2>Czwarty slajd</h2><button> Kliknij se!</button></li>
-      </ul>
-    </section>
+    return <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="./../../../images/first.png" alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h3>Pierwszy Slajd</h3>
+            <p><button> Kliknij se!</button></p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="./../../../images/second.png" alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h3>Drugi Slajd</h3>
+            <p><button> Kliknij se!</button></p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="./../../../images/third.png" alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h3>Trzeci Slajd</h3>
+            <p><button> Kliknij se!</button></p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="./../../../images/fourth.png" alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h3>Czwarty Slajd</h3>
+            <p><button> Kliknij se!</button></p>
+          </div>
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+   </div>
+
   }
 }
 export default Slider
